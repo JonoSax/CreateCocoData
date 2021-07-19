cff65b4ae17755c07874213ff3383e6b4b8961e8
Jonathan Reshef <jono.reshef@gmail.com>
2021-07-19 16:05:02 +1200

    improved to be easier to use and updated for new coco inputs

--- overlaySegment.py
+++ overlaySegment.py
@@ -5,6 +5,7 @@ import cv2
 from multiprocessing import Process
 import multiprocessing
 import os
+from utilities import createIDDict
 
 def drawLine(img, point0, point1, blur = 2, colour = [0, 0, 255]):
 
@@ -37,25 +38,6 @@ def drawLine(img, point0, point1, blur = 2, colour = [0, 0, 255]):
     
     return(img)
 
-def createIDDict(targetdict, keytype, classtype, pathsrc = ""):
-
-    '''
-    Create a dictionary which extracts information from within a list of dictionaries
-    '''
-
-    imgDict = {}
-
-    for i in targetdict:
-        key = i[keytype]
-        if classtype == "*":
-            if imgDict.get(key) is None:
-                imgDict[key] = []
-            imgDict[key].append(i)
-        else:
-            imgDict[key] = pathsrc + str(i[classtype])
-
-    return(imgDict)
-
 def getSegPos(seg):
 
     '''
@@ -76,34 +58,30 @@ def getSegPos(seg):
 
     return(pix)
 
-if __name__ == "__main__":
+def annotateSegments(src, random = True):
 
-    imgsrc = "data/coco/images/"
-    segsrc = "data/coco/annotations/instances_train2017.json"
+    '''
+    Load in the coco information and annotate the segments
 
+    Inputs   
+    src:        the coco file containing the image and segment info
+    random      if set to a value then will randomally n number of annotations
 
-    imgsrc = "data/datasources/testdataset/fish/"
-    segsrc = "data/datasources/testdataset/cocoInfo_Val.json"
+    '''
 
-    imgsrc = "/Volumes/USB/segmentationData/instance/Fish4Knowledge/testdataset2/fish/"
-    segsrc = imgsrc + "train.json"
-
-    imgsrc = "/Volumes/USB/segmentationData/dataGenerator/foregrounds/modImages/img/"
-    segsrc = imgsrc + "train.json"
-
-
-    segFile = open(segsrc)
-    segments = json.load(segFile)
-
+    segments = json.load(open(src, "r"))
     annotations = segments["annotations"]
     images = segments["images"]
     categories = segments["categories"]
 
     # downloadImages(imgsrc, images, limit = 10)
-    imgDict = createIDDict(images, "id", "file_name", imgsrc)
+    imgDict = createIDDict(images, "id", "file_name")
     # imgDict = createIDDict(images, "id", "file_name")
     categories = createIDDict(categories, "id", "name")
 
+    keys = list(imgDict.keys())[-10:]
+    [imgDict[k] for k in keys]
+    randomCount = 0
 
     for n, a in enumerate(annotations):
         id = a["image_id"]
@@ -113,35 +91,45 @@ if __name__ == "__main__":
             continue
 
         cat = categories[catid]
-        img = cv2.imread(imgpath)
+        img = cv2.imread(imgpath[0])
 
         if img is None:
             continue
+        elif randomCount >= random:
+            return
 
         print(f"ID = {cat}")
         imgm = img.copy()
-        imgm = (imgm.astype(float)*0.2).astype(np.uint8)
+        # imgm = (imgm.astype(float)*0.2).astype(np.uint8)
         for s in a["segmentation"]:
-            x0, y0, x1, y1 = a["bbox"]
-            x1+=x0
-            y1+=y0
             pix = getSegPos(s)
             '''
             for p in pix:
                 imgm[p[0], p[1]] = [0, 0, 255]
             '''
-            
+            # draw the outline of the annotation
             for p0, p1 in zip(pix, np.vstack([pix[1:], pix[0]])):
                 try:
                     imgm = drawLine(imgm, p0, p1)
                 except:
                     pass
-            
-            imgm = drawLine(imgm, np.array([x0, y0]), np.array([x1, y0]))
-            imgm = drawLine(imgm, np.array([x0, y0]), np.array([x0, y1]))
-            imgm = drawLine(imgm, np.array([x1, y1]), np.array([x1, y0]))
-            imgm = drawLine(imgm, np.array([x1, y1]), np.array([x0, y1]))
+
+        # draw the bounding box
+        x0, y0, x1, y1 = a["bbox"]
+        x1+=x0
+        y1+=y0
+        imgm = drawLine(imgm, np.array([x0, y0]), np.array([x1, y0]))
+        imgm = drawLine(imgm, np.array([x0, y0]), np.array([x0, y1]))
+        imgm = drawLine(imgm, np.array([x1, y1]), np.array([x1, y0]))
+        imgm = drawLine(imgm, np.array([x1, y1]), np.array([x0, y1]))
         cv2.imshow("img", np.hstack([img, imgm])); cv2.waitKey(0)
+
+        randomCount += 1
 
         # y0, x0, y1, x1 = np.array(bbox.replace("[", "").replace("]", "").replace(" ", "").split(",")).astype(int)
         # cv2.rectangle(imgm, [x0, y0], [x0+x1, y0+y1], [0, 0, 255], 2)
+
+if __name__ == "__main__":
+
+    segsrc = "/Volumes/WorkStorage/BoxFish/dataStore/fishData/YOLO_data/testCoco.json"
+    annotateSegments(segsrc, random = 10)
