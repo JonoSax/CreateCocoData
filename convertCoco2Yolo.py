f75585fe03e0ac741ac5f55d11034f27c26d8ad3
Jonathan Reshef <jono.reshef@gmail.com>
2021-08-12 18:18:06 +1200

    converts the coco data set into a yolo data set and file format structure

---
+++ convertCoco2Yolo.py
@@ -0,0 +1,112 @@
+'''
+Take the coco structure and convert it to yolo structure
+'''
+
+import json
+import os
+from cocoDataStructure.utilities import createIDDict, dirMaker, printProgressBar
+from multiprocessing import Pool
+from itertools import repeat
+from random import shuffle
+from overlaySegment import annotateYoloSegments
+
+def createYoloData(src, dest):
+
+    '''
+    Take the coco data and convert into yolo
+
+    YOLO data strcuture:
+
+    data 
+        images
+            train
+                img0.png
+                img1.png
+                ...
+            test
+                imgn.png
+            val
+                imgn.png
+        labels
+            train
+                img0.txt
+                img1.txt
+                ...
+            test
+                imgn.txt
+            val
+                imgn.txt
+
+    imgn.txt:
+    [class ID] [xCenter%] [yCenter%] [w%] [h%]
+    
+    '''
+
+    dataType = src.split("/")[-1].split(".")[0]
+
+    cocoData = json.load(open(src))
+
+    labelDest = dest + "labels/" + dataType + "/"
+    imgDest = dest + "images/" + dataType + "/"
+
+    dirMaker(labelDest, True)
+    dirMaker(imgDest, True)
+
+    images = cocoData["images"][35000:]
+    # shuffle(images)
+    images = images[:100]
+    annos = cocoData["annotations"]
+
+    # create the dictionary which relates the image ids to their anno ids
+    annoIds = createIDDict(annos, "image_id", "*")
+
+    with Pool(4) as p:
+        p.starmap(convertData, zip(images, repeat(annos), repeat(annoIds), repeat(labelDest), repeat(imgDest)))
+
+def convertData(i, annos, annoIds, labelDest, imgDest):
+
+    imgPath = i["file_name"]
+    imgName = imgPath.split("/")[-1].split(".")[0]
+    imgId = i["id"]
+    img_h = i["height"]
+    img_w = i["width"]
+
+    f = open(os.path.join(labelDest, imgName + ".txt"), "w")
+
+    # copy the image to the new destination
+    os.system("cp " + imgPath + " " + imgDest)
+
+    imgAnno = annoIds[imgId]
+
+    for a in imgAnno:
+        x, y, h, w = a["bbox"]
+        label = a["category_id"]
+
+        # get the centre of the annotation
+        x += h/2
+        y += w/2
+
+        # normalise the coordinates
+        x /= img_w
+        y /= img_h
+        h /= img_w
+        w /= img_h
+
+        f.write(f"{label} {x} {y} {h} {w}\n")
+
+    f.close()
+
+
+if __name__ == "__main__":
+
+    srcs = ["/Volumes/USB/data/CocoData/train.json",
+    "/Volumes/USB/data/CocoData/val.json"]
+
+    dest = "/Volumes/USB/data/YoloData/"
+
+    for src in srcs:
+        createYoloData(src, dest)
+
+    yolosrc = "/Volumes/USB/data/YoloData/"
+
+    annotateYoloSegments(yolosrc, False)
