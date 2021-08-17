23585e808dbab5762d2e34c90d1042b6a13c8003
Jonathan Reshef <jono.reshef@gmail.com>
2021-08-17 13:28:59 +1200

    some changes...

--- createSourceInfo.py
+++ createSourceInfo.py
@@ -16,26 +16,29 @@ import os
 
 if __name__ == "__main__":
 
-    cpuNo = 4
+    cpuNo = 1
     
     wd = "/Volumes/WorkStorage/BoxFish/dataStore/fishData/CocoData/"
     wd = "/Volumes/USB/data/CocoData/"
+    wd = "/media/boxfish/USB/data/CocoData/"
 
     srcs = [wd + "Ulucan/",
     wd + "Fish4Knowledge/",
     wd + "openimages/",
-    wd + "QUT/"]
+    wd + "QUT/", 
+    wd + "NorFisk_v1.0/"]
 
     # srcs = ["/Volumes/WorkStorage/BoxFish/dataStore/Aruco+Net/net_day_shade_pool/"]
     # srcs = ["/Volumes/WorkStorage/BoxFish/dataStore/netData/foregrounds/mod/"]
 
-    '''
-    for s in srcs:
-        associateImageID(s)
-        getImageInfo(s)
-        getAnnotationInfo(s)
-    '''
-    with Pool(cpuNo) as pool:
-        pool.map(associateImageID, srcs)
-        pool.map(getImageInfo, srcs)
-        pool.map(getAnnotationInfo, srcs)
+    if cpuNo == 1:
+        for s in srcs[-1:]:
+            associateImageID(s)
+            getImageInfo(s)
+            getAnnotationInfo(s)
+    else:
+        # NOTE parallelise the individual functions
+        with Pool(cpuNo) as pool:
+            pool.map(associateImageID, srcs)
+            pool.map(getImageInfo, srcs, True)
+            # pool.map(getAnnotationInfo, srcs)
